8525f43a54c3e95791a229a0e9aad46baf8e874a
karlbennett <where_1ts@yahoo.com>
2013-01-04 00:13:58 +0000

    Created a new UserRepository that will be used to persist Users.

---
+++ src/main/java/scratch/webapp/data/UserRepository.java
@@ -0,0 +1,12 @@
+package scratch.webapp.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * A CRUD repository for the {@link User} class. It extends from the Spring Data {@link CrudRepository} which means it's
+ * implementation will be generated on application start.
+ *
+ * @author Karl Bennett
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+}
