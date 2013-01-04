ad7c44df12891f579a5d4dd76aac4bcb7b3234ff
Karl Bennett <karl.bennett@bbc.co.uk>
2013-01-04 10:16:03 +0000

    Removed unused import from ScratchController class.

--- src/main/java/scratch/webapp/controller/ScratchController.java
+++ src/main/java/scratch/webapp/controller/ScratchController.java
@@ -2,7 +2,6 @@ package scratch.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
