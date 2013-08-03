f263f790dc20e9a24129f0d85add60192f267031
Karl Bennett <karlbennett@orcon.net.nz>
2013-08-03 18:48:17 +1200

    Removed the web.xml and created a custom ScratchDispatcherServlet class that is now registered with the @WebServlet annotation.

---
+++ src/main/java/scratch/spring/webapp/servlet/ScratchDispatcherServlet.java
@@ -0,0 +1,25 @@
+package scratch.spring.webapp.servlet;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * @author Karl Bennett
+ */
+@WebServlet(
+        value = "/scratch/*",
+        initParams = {
+                @WebInitParam(
+                        name = "contextClass",
+                        value = "org.springframework.web.context.support.AnnotationConfigWebApplicationContext"
+                ),
+                @WebInitParam(
+                        name = "contextConfigLocation",
+                        value = "scratch.spring.webapp.config.ScratchConfiguration"
+                )},
+        asyncSupported = true
+)
+public class ScratchDispatcherServlet extends DispatcherServlet {
+}
